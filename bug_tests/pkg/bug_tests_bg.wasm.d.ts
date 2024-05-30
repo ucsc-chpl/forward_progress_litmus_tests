abf3dd9e0a771991890475ad90866f8d0c916913
naomi <naomi.rehman1008@gmail.com>
2024-05-30 13:55:54 -0700

    rebuilt wasm for the bug tests

---
+++ bug_tests/pkg/bug_tests_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function run(a: number, b: number, c: number): number;
+export function execute_gpu(a: number, b: number, c: number): number;
+export function get_gpu_name(): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function wasm_bindgen__convert__closures__invoke1_mut__h125383f9b0357508(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h9ec33571aa62dd8d(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h064d8358ebdccb23(a: number, b: number, c: number, d: number): void;
